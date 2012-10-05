e0c43aaa705e1266cbeb1f40e8b495c722c2fee9
Stephen O'Flynn <flynniec6@gmail.com>
2012-10-05 14:09:58 +0200

    Basic error checking and results area
    Only checks for blank words

--- js/wordchain.js
+++ js/wordchain.js
@@ -1,15 +1,63 @@
 $(function() {
     // document ready
-    $('#iChainButton').click(function() {
-    	var sStart = $('#iStartWord').val();
-    	var sEnd   = $('#iEndWord').val();
-        chainWords(sStart, sEnd);
+    $('#iStartWord').focus(function() {
+        $('#dStartWord').removeClass('error');
+        $('#mStartWord').text('');
     });
+
+    $('#iEndWord').focus(function() {
+        $('#dEndWord').removeClass('error');
+        $('#mEndWord').text('');
+    });
+
+    $('#iChainButton').click(function() {
+        checkInputs();
+    });
+    
+    $('input').keypress(function(e) {
+        if(e.which == 13) {
+        	checkInputs();
+        }
+    });
+
+	$('#iStartWord').focus();
 });
 
-function chainWords(sStart, sEnd) {
-	var msg = 'Chainy chainy!';
-	msg = msg + '\nstart: ' + sStart;
-	msg = msg + '\nend: ' + sEnd;
-	alert(msg);
+function checkInputs() {
+	var sStart = $('#iStartWord').val();
+	var msgStart = "";
+	var errStart = false;
+	if (sStart == "") { 
+		msgStart = "Starting word required";
+		errStart = true;
+	}
+		
+	var sEnd   = $('#iEndWord').val();
+	var msgEnd = "";
+	var errEnd = false;
+	if (sEnd == "") { 
+		msgEnd = "Ending word required";
+		errEnd = true;
+	}
+	
+	// update screen
+	if (errStart || errEnd) {
+		if (errStart) {
+			$('#mStartWord').text(msgStart);
+			$('#dStartWord').addClass("error");
+		}
+	
+		if (errEnd) {
+			$('#mEndWord').text(msgEnd);
+			$('#dEndWord').addClass("error");
+		}
+	}
+	else {	// no errors in input
+		chainWords(sStart, sEnd);
+	}
 }
+
+function chainWords(start, end) {
+	$('#chainResults').html('<p>Some results appear here.');
+}
+
